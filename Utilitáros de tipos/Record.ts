49d70040ecaba1a8313c4eee8e5df686fda49533
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-03-22 20:09:08 -0300

    Utilitário de tipo Record

---
+++ Utilitáros de tipos/Record.ts
@@ -0,0 +1,24 @@
+//criando objeto onde todas as chaves são strings e os valores sao numbers
+const scores: Record<string, number> = {
+    "John": 50,
+    "Jane": 75
+}
+
+//Limita valores
+type Prfile = "admin" | "user" | "guest"
+const user: Record<Prfile, number> = {
+    "admin": 0,
+    "user": 1,
+    "guest": 2
+}
+
+//Objetos personalizados
+interface User {
+    name: string
+    email: string
+}
+
+const users: Record<number, User> = {
+    1: { name: "John", email: "john@example" },
+    2: { name: "Jane", email: "jane@example" }
+}
