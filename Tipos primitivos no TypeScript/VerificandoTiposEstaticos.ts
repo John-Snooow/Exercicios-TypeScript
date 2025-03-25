b4782ec9d1e4322fd36c513ce832418e00ba1f3f
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-03-24 23:10:19 -0300

    Verificação de tipo estático

---
+++ Tipos primitivos no TypeScript/VerificandoTiposEstaticos.ts
@@ -0,0 +1,22 @@
+// const message = "Hello TypeScript!"
+let message = "Hello TypeScript!"
+
+function showMessage() {
+    console.log(message)
+}
+
+// Consigo mudar?
+message = "TypeScript!"
+
+// Consigo executar?
+// message()
+showMessage()
+
+const user = {
+    name: "Rodrigo Gonçalves",
+    email: "rodrigo@email.com"
+}
+
+// Propriedade existe?
+// console.log(user.avatar)
+console.log(user.name)
