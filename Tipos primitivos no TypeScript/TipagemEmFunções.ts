d16bd3127df7677cc014f2db24c548a43e51b83d
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-03-25 22:58:55 -0300

    Tipagem em Funções

---
+++ Tipos primitivos no TypeScript/TipagemEmFunções.ts
@@ -0,0 +1,16 @@
+function sum (x: number, y: number): number {
+    const result = x + y
+    console.log("Resultado = " + result)
+
+    return result
+}
+
+const result = sum(7, 3)
+
+const showMessage = (name: string): string => {
+    const message = "Olá " + name
+
+    return message
+}
+
+showMessage("Rodrigo Gonçalves")
