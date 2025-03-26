69a303f71ac6540cbca103f399c817b40b120e3a
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-03-25 23:07:12 -0300

    fazendo comentarios

--- Tipos primitivos no TypeScript/TipagemEmFunções.ts
+++ Tipos primitivos no TypeScript/TipagemEmFunções.ts
@@ -1,16 +1,18 @@
+// Função com tipagem explícita
 function sum (x: number, y: number): number {
     const result = x + y
     console.log("Resultado = " + result)
-
+    // O retorno da função é o tipo number
     return result
 }
-
+// Chamando a função com os parâmetros necessários
 const result = sum(7, 3)
 
+// Função com tipagem implícita
 const showMessage = (name: string): string => {
     const message = "Olá " + name
-
+    // O retorno da função é o tipo string
     return message
 }
-
+// Chamando a função com o parâmetro necessário
 showMessage("Rodrigo Gonçalves")
