45e729f15f0143ccf29fe4780bd8bca4a3b84b88
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-03-25 22:55:08 -0300

    Tipagem em Arrays

---
+++ Tipos primitivos no TypeScript/TipagemEmArrays.ts
@@ -0,0 +1,6 @@
+// Tipagem explícita ou Anotação de tipo (Type Annotation)
+let names: string[] = ["Rodrigo", "Mayk"]
+let numbers: number[] = [1, 2, 3, 4, 5]
+
+// Inferência de tipo.
+let products = ["Product x", "Product y", "Product z"]
